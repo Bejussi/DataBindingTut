9f55737b554c161e6b351838f403f5fab313ceaa
Alexandra <pindooqy@gmail.com>
2022-05-19 12:02:47 +0300

    Learning basic data binding

--- app/src/main/java/com/bejussi/example/databindingtut/SimpleExample.kt
+++ app/src/main/java/com/bejussi/example/databindingtut/SimpleExample.kt
@@ -2,19 +2,23 @@ package com.bejussi.example.databindingtut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.bejussi.example.databindingtut.data.SampleWeatherData
+import com.bejussi.example.databindingtut.databinding.ActivitySimpleExampleBinding
 
 
 class SimpleExample : AppCompatActivity() {
     // TODO: create an instance of the data class
-
+    val theWeather = SampleWeatherData(30,"Overcast",5,"C")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // TODO: inflate the layout using the binding library
-
+        val binding: ActivitySimpleExampleBinding =
+            DataBindingUtil.setContentView(this,R.layout.activity_simple_example)
 
         // TODO: connect the data with the layout variable
-
+        binding.weather = theWeather
     }
 }
